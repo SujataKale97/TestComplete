5cb94b46426b29a8d09376a14057bb5d1bd9381a
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2020-02-26 18:31:25 +0530

    Create jenkinsfile1.groovy

---
+++ jenkinsfile1.groovy
@@ -0,0 +1,11 @@
+node("slave1")
+{
+  stage('Code Clone')
+  {
+   git 'https://github.com/SujataKale97/TestComplete.git'
+  }
+  stage('Test')
+  {
+    testcompletetest suite: 'Checkpoints/Web Accessibility/WebAccessibilityCheckpoints_JavaScript/WebAccessibilityCheckpoints_JavaScript.pjs'
+  }
+}
